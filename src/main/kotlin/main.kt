5475f2553cfe1e83108ae20be1e5b4795e10b5a2
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-08 17:43:00 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,11 @@ fun main() {
     val przemyslaw = Student("Przemysław","Stokłosa")
     val piotr = Student("Piotr","Stokłosa")
 
+    piotr.wplacKwote(100.0)
+
     przemyslaw.printStudent()
+    piotr.printStudent()
+
+    piotr.wplacKwote(1000.0)
     piotr.printStudent()
 }
