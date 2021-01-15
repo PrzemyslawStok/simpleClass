d7724719dcd31df284820a87dbb3bf2fcf2755f5
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-15 16:32:42 +0100

    cd..

--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -1,1 +1,5 @@
-class Student
+class Student{
+    val imie = "Przemysław"
+    val nazwisko = "Stokłosa"
+    var id = "23322"
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,10 @@
 fun main() {
     val przemyslaw = Student()
+    val piotr = Student()
+
     println(przemyslaw)
+    println(przemyslaw.toString())
+
+    println(przemyslaw.imie)
+    println(piotr.imie)
 }
